4b16b175a2dae8ad44430a4c6f3c53269b90ba51
moto <motochan1969@gmail.com>
2021-04-13 23:54:06 +0900

    init

--- dict_page/translate.py
+++ dict_page/translate.py
@@ -2,7 +2,7 @@ import os, requests, uuid, json
 
 # Don't forget to replace with your Cog Services subscription key!
 # If you prefer to use environment variables, see Extra Credit for more info.
-subscription_key = '0d8390353d9d46ea92b38d804fc0adc2'
+from . import config
 
 # Our Flask route will supply two arguments: text_input and language_output.
 # When the translate text button is pressed in our Flask app, the Ajax request
